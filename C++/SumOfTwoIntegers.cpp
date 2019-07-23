85c8b9d34b526fb0110877a9c0ff4315063cadab
georgesuarez <georgejsuarez@gmail.com>
2019-07-22 18:05:25 -0700

    Fixed small error in InorderTraversalBST.cpp/Added solution for SumOfTwoIntegers problem

---
+++ C++/SumOfTwoIntegers.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int getSum(int a, int b)
+    {
+
+        while (b != 0)
+        {
+            int carry = a & b;
+            a ^= b;
+            b = (unsigned int)carry << 1;
+        }
+
+        return a;
+    }
+};
